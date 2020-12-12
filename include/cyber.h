7261b82985ecea11d6ab5fe79d0165a7bd35e52b
Tamir Cohen <Tamircohen2468@gmail.com>
2020-12-12 22:14:41 +0200

    update

---
+++ include/cyber.h
@@ -0,0 +1,5 @@
+#include "stdbool.h"
+
+bool func1(int a);
+
+bool func2(int a);
---
+++ src/cyber.c
@@ -0,0 +1,9 @@
+#include "../include/cyber.h"
+
+bool func1(int a) {
+    return a > 10;
+}
+
+bool func2(int a) {
+    return a > 100;
+}
